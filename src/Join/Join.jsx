685010ab53e3b27b103be799952229505537b478
Zac W <zacwalters4@gmail.com>
2025-01-24 19:05:55 -0700

    Add glow to join section logo

--- src/Join/Join.jsx
+++ src/Join/Join.jsx
@@ -8,8 +8,8 @@ const Join = () => {
         <div className='pt-28 w-full flex flex-col justify-center items-center'>
                 <div className='lg:w-[60%] w-full h-[400px] bg-gradient-background-3 lg:rounded-[20px] bg-black'>
                     <div className='w-full h-full bg-cover flex flex-col justify-center items-center' style={{backgroundImage: `url(${bardFront})`}}>
-                        <img src={bardLogo} alt='Bard Signals Logo' className='h-40'/>
-                        <h1 className='md:text-6xl text-3xl font-extrabold bg-text-gradient bg-clip-text text-transparent text-center'>Join Bard Today</h1>
+                        <img src={bardLogo} alt='Bard Signals Logo' className='h-40 drop-shadow-logo-drop-shadow'/>
+                        <h1 className='md:text-6xl text-3xl font-extrabold bg-text-gradient bg-clip-text text-transparent text-center pb-2'>Join Bard Signals Today</h1>
                         <h2 className='text-[#b3b3b3] mt-2 mb-4 md:text-lg text-sm text-center w-[95%]'>The #1 confluence, wallet, and criteria-based signal bot on Solana</h2>
                         <JoinButton />
                     </div>
