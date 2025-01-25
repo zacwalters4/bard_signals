b8ae3a7c12c96dd83791c60a232f85b06894856c
Zac W <zacwalters4@gmail.com>
2025-01-24 19:02:40 -0700

    Fix background going through join section

--- src/Join/Join.jsx
+++ src/Join/Join.jsx
@@ -5,8 +5,8 @@ import bardFront from '../Images/bard_logo_front.png'
 const Join = () => {
 
     return (
-        <div className='pt-24 w-full flex flex-col justify-center items-center'>
-                <div className='lg:w-[60%] w-full h-[400px] bg-gradient-background-3 lg:rounded-[20px]'>
+        <div className='pt-28 w-full flex flex-col justify-center items-center'>
+                <div className='lg:w-[60%] w-full h-[400px] bg-gradient-background-3 lg:rounded-[20px] bg-black'>
                     <div className='w-full h-full bg-cover flex flex-col justify-center items-center' style={{backgroundImage: `url(${bardFront})`}}>
                         <img src={bardLogo} alt='Bard Signals Logo' className='h-40'/>
                         <h1 className='md:text-6xl text-3xl font-extrabold bg-text-gradient bg-clip-text text-transparent text-center'>Join Bard Today</h1>
