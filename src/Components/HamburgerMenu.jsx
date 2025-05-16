f163b1613bd08f1c1d2b4a05ec985f8d31c8f7ea
Zac W <zacwalters4@gmail.com>
2025-05-16 02:11:38 -0600

    Add header items

--- src/Components/HamburgerMenu.jsx
+++ src/Components/HamburgerMenu.jsx
@@ -2,7 +2,7 @@
 const HamburgerMenu = ({toggleDropdown, dropdownShow}) => {
 
     return (
-        <button onClick={toggleDropdown} className='lg:hidden pr-3'>
+        <button onClick={toggleDropdown} className='xl:hidden pr-3'>
             <div className='grid grid-cols-1 grid-rows-3 w-6 h-6 place-items-center '>
                 <div className={`transition duration-300 h-[2px] w-6 bg-gradient-background ${dropdownShow && 'transform rotate-45 translate-y-2'}`}></div>
                 <div className={`transition duration-300 h-[2px] w-6 bg-gradient-background ${dropdownShow && 'transform -rotate-45'}`}></div>
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
                             <img src={bardLogo} alt='Bard Signals' className='ml-2 h-8'/>
                             <h1 className='pl-2 text-2xl bg-text-gradient bg-clip-text text-transparent'>Bard Signals</h1>
                         </div>
-                        <div className='w-[20%] hidden lg:flex justify-evenly items-center'>
+                        <div className='w-[40%] hidden xl:flex justify-evenly items-center'>
                             <a href='#profits'>
                                 <button className='bg-text-gradient bg-clip-text text-transparent hover:bg-white text-sm'>
                                     <h3>Profits</h3>
@@ -35,8 +35,19 @@ const Header = () => {
                                     <h3>Pricing</h3>
                                 </button>
                             </a>
+                            <a href='https://docs.bardsignals.com/'>
+                                <button className='bg-text-gradient bg-clip-text text-transparent hover:bg-white text-sm'>
+                                    <h3>Documentation</h3>
+                                </button>
+                            </a>
+                            <a href='https://dexscreener.com/solana/7f6aledjcnd22uk6xwop2tmn1igndt74ptz3ijgiumu5'>
+                                <button className='bg-text-gradient bg-clip-text text-transparent hover:bg-white text-sm'>
+                                    <h3>$BARD Token</h3>
+                                </button>
+                            </a>
+                            
                         </div>
-                        <div className='hidden lg:flex flex-row justify-between items-center h-full  mr-2'>
+                        <div className='hidden xl:flex flex-row justify-between items-center h-full  mr-2'>
                             <a href='https://discord.gg/bardsignals' className='h-8 w-8 flex justify-center items-center border-[1px] border-[#8a6df2] rounded-[2.5rem] overflow-hidden mr-2'>
                                 <img src={discordLogo} className='h-[20px] border-solid' />
                             </a>
@@ -57,6 +68,18 @@ const Header = () => {
                         <a href='#pricing' className='w-full'>
                             <button onClick={toggleDropdown} className='bg-text-gradient bg-clip-text text-transparent hover:bg-white text-lg h-10 w-full'>
                                 <h3>Pricing</h3>
+                            </button>
+                        </a>
+                        <div className='bg-border-gradient h-[1px] w-full my-2'/>
+                        <a href='https://docs.bardsignals.com/' className='w-full'>
+                            <button onClick={toggleDropdown} className='bg-text-gradient bg-clip-text text-transparent hover:bg-white text-lg h-10 w-full'>
+                                <h3>Documentation</h3>
+                            </button>
+                        </a>
+                        <div className='bg-border-gradient h-[1px] w-full my-2'/>
+                        <a href='https://dexscreener.com/solana/7f6aledjcnd22uk6xwop2tmn1igndt74ptz3ijgiumu5' className='w-full'>
+                            <button onClick={toggleDropdown} className='bg-text-gradient bg-clip-text text-transparent hover:bg-white text-lg h-10 w-full'>
+                                <h3>$BARD Token</h3>
                             </button>
                         </a>
                         <div className='bg-border-gradient h-[1px] w-full my-2'/>
